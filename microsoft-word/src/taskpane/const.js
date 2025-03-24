5b5b5c2d725da79ccfba4826e39eba96a0ad08c6
thiennbao <thiennbao.204@gmail.com>
2025-03-24 11:24:29 +0700

    add language options

---
+++ microsoft-word/src/taskpane/const.js
@@ -0,0 +1,15 @@
+export const languages = [
+  "Abkhaz", "Acehnese", "Acholi", "Afar", "Afrikaans", "Albanian", "Amharic", "Arabic", "Armenian", "Azerbaijani",
+  "Basque", "Belarusian", "Bengali", "Bhojpuri", "Bosnian", "Bulgarian", "Burmese", "Catalan", "Cebuano", "Chichewa",
+  "Chinese (Simplified)", "Chinese (Traditional)", "Corsican", "Croatian", "Czech", "Danish", "Dutch", "Dzongkha",
+  "English", "Esperanto", "Estonian", "Ewe", "Fijian", "Finnish", "French", "Galician", "Georgian", "German",
+  "Greek", "Guarani", "Gujarati", "Haitian Creole", "Hausa", "Hawaiian", "Hebrew", "Hmong", "Hungarian", "Icelandic",
+  "Igbo", "Ilokano", "Indonesian", "Inuktitut", "Irish", "Italian", "Japanese", "Javanese", "Kannada", "Kazakh",
+  "Khmer", "Kinyarwanda", "Klingon", "Korean", "Kurdish", "Kyrgyz", "Lao", "Latin", "Latvian", "Lingala",
+  "Lithuanian", "Luxembourgish", "Macedonian", "Maori", "Marathi", "Mayan", "Mizo", "Mongolian", "Nepali", "Nigerian Pidgin",
+  "Nko", "Norwegian", "Nyanja", "Odia", "Pashto", "Persian", "Polish", "Portuguese", "Punjabi", "Quechua",
+  "Romanian", "Russian", "Samoan", "Scots Gaelic", "Serbian", "Sesotho", "Shona", "Sindhi", "Sinhala", "Slovak",
+  "Slovenian", "Somali", "Southern Sotho", "Spanish", "Sundanese", "Swahili", "Swati", "Swedish", "Tajik", "Tamil",
+  "Tatar", "Telugu", "Thai", "Tigrinya", "Tongan", "Turkish", "Turkmen", "Ukrainian", "Urdu", "Uyghur",
+  "Uzbek", "Vietnamese", "Welsh", "Xhosa", "Yiddish", "Yoruba", "Zulu",
+];
--- microsoft-word/src/taskpane/taskpane.js
+++ microsoft-word/src/taskpane/taskpane.js
@@ -1,17 +1,48 @@
+import { languages } from "./const";
+
+var sourceLang = "Auto detect";
+var targetLang = "Vietnamese";
+
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("app-load").style.display = "none";
     document.getElementById("app-body").style.display = "block";
 
+    // Append language options
+    document.getElementById("source-lang").appendChild(createOptionElm("Auto detect", true));
+    document.querySelectorAll("select[id$=lang]").forEach((select) => {
+      languages.forEach((lang) => {
+        select.appendChild(createOptionElm(lang));
+      });
+    });
+    document.querySelectorAll("#target-lang>option[value=Vietnamese]")[0].selected = true;
+
     // Events register
-    Office.context.document.addHandlerAsync(Office.EventType.DocumentSelectionChanged, onSelect);
+    document.getElementById("source-lang").addEventListener("change", onSelectSource);
+    document.getElementById("target-lang").addEventListener("change", onSelectTarget);
+    Office.context.document.addHandlerAsync(
+      Office.EventType.DocumentSelectionChanged,
+      onSelectText
+    );
   }
 });
 
-async function onSelect() {
-  Word.run(async (context) => {
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
-    paragraph.font.color = "blue";
-    context.sync();
-  });
+async function onSelectSource(event) {
+  sourceLang = event.target.value;
+}
+async function onSelectTarget(event) {
+  targetLang = event.target.value;
+}
+
+async function onSelectText() {
+  document.getElementById("source").value = `${sourceLang}: Lmao`;
+  document.getElementById("target").innerText = `${targetLang}: Lmao`;
+}
+
+function createOptionElm(value, selected = false) {
+  const option = document.createElement("option");
+  option.value = value;
+  option.textContent = value;
+  option.selected = selected;
+  return option;
 }
