05844da49cc1e1b5dfac06b1deca49cbc5507a0e
thiennbao <thiennbao.204@gmail.com>
2025-03-25 15:50:42 +0700

    add settings

--- microsoft-word/src/taskpane/taskpane.js
+++ microsoft-word/src/taskpane/taskpane.js
@@ -25,13 +25,14 @@ Office.onReady((info) => {
     $("#app-load").css("display", "none");
     $("#app-body").css("display", "flex");
 
-    // Append language options
+    // Append options and set default values
     $("#source-lang").append(createOptionElm("Auto detect", true));
     languages.forEach((lang) => {
       $("#source-lang").append(createOptionElm(lang));
       $("#target-lang").append(createOptionElm(lang));
     });
     $("#target-lang>option[value=Vietnamese]")[0].selected = true;
+    $("#temperature").val(0.8);
 
     // Events register
     window.addEventListener("stateUpdate", onStateUpdated);
@@ -39,6 +40,9 @@ Office.onReady((info) => {
     $("#target-lang").on("change", onTargetLangChange);
     $("#swap-btn").on("click", onSwapLang);
     $("#source").on("input", onTyping);
+    $("#model-name").on("change", onModelChange);
+    $("#temperature").on("input", onTempChange);
+    $("#writting-style").on("change", onStyleChange);
     Office.context.document.addHandlerAsync(
       Office.EventType.DocumentSelectionChanged,
       onSelectText
@@ -50,10 +54,16 @@ Office.onReady((info) => {
 async function onStateUpdated() {
   if (debounceTimer) clearTimeout(debounceTimer);
   if (stateManager.state.text) {
+    // 2-ways binding
     $("#source-lang").val(stateManager.state.sourceLang);
     $("#target-lang").val(stateManager.state.targetLang);
     $("#source").val(stateManager.state.text);
+    $("#model-name").val(stateManager.state.modelName);
+    $("#tempurature").val(stateManager.state.temperature);
+    $("#writting-style").val(stateManager.state.wrttingStyle);
+    // Insert translation
     const translation = await translate(stateManager.state);
+    console.log(stateManager.state, translation);
     $("#target").val(translation);
   }
 }
@@ -61,7 +71,7 @@ function onSourceLangChange(e) {
   stateManager.setState({ ...stateManager.state, sourceLang: e.target.value });
 }
 function onTargetLangChange(e) {
-  stateManager.setState({ ...stateManager.state, sourceLang: e.target.value });
+  stateManager.setState({ ...stateManager.state, targetLang: e.target.value });
 }
 function onSwapLang() {
   const currentSource = stateManager.state.sourceLang;
@@ -74,6 +84,15 @@ function onSwapLang() {
 }
 function onTyping(e) {
   stateManager.setState({ ...stateManager.state, text: e.target.value });
+}
+function onModelChange(e) {
+  stateManager.setState({ ...stateManager.state, modelName: e.target.value });
+}
+function onTempChange(e) {
+  stateManager.setState({ ...stateManager.state, temperature: e.target.value });
+}
+function onStyleChange(e) {
+  stateManager.setState({ ...stateManager.state, wrttingStyle: e.target.value });
 }
 async function onSelectText() {
   await Word.run(async (context) => {
